e7477005e51ac040cc766731f6e6c84491d437c2
kimrobote <kimrobote768@gmail.com>
2024-09-07 17:20:49 +0900

    유저 가입 정보 등록 기능 개발 중

---
+++ src/main/java/kr/co/kim/model/User.java
@@ -0,0 +1,14 @@
+package kr.co.kim.model;
+
+public class User {
+    private String id;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    private String password;
+    private String name;
+    private String email;
+}
